7b8fd002e63a955bbe2f610478727eb44c3a0016
cristianorregod <cristian.duquew@gmail.com>
2025-06-09 07:23:28 -0500

    Fix: Ajusta el logo, estilos

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,22 +1,15 @@
+export const Header = ({ title = "Perfil del cliente" }) => {
+  return (
+    <header className="mb-10">
+      <div className="w-full ">
+        <img
+          className="mx-auto w-80 rounded-b-2xl p-4 shadow-[14px_16px_16px_-9px_#0000001a,inset_15px_-15px_18px_-10px_#0000001a]"
+          src="/assets/colraices_logo.svg"
+          alt="Logo Colraices"
+        />
+      </div>
 
-export const Header = ({title = "Perfil del cliente"}) => {
-    return (
-
-        <header className="mb-10">
-            <div className="p-8 w-full">
-                
-                    <img style={{
-                        margin:'0 auto',
-                        width:'400px',
-                        height:'auto'
-                    }} src="/assets/colraices_logo.svg" alt="Logo Colraices" />
-            
-            </div>
-
-            <h1 className='title mb-10'>{title}</h1>
-
-        </header>
-
-
-    )
-}
+      <h1 className="title mb-10">{title}</h1>
+    </header>
+  );
+};
